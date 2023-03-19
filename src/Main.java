fef605d84dff59fca90c4bed9ef824ba238fc4a9
Riad <riadcosic@outlook.com>
2023-03-19 18:50:59 +0100

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,74 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /* Zadatak 4.
+       String randomText = "random text";
+        int randomNumber = randomText;
+
+        int randomNumber2 = 2;
+        String randomText2 = randomNumber2;
+
+        char randomChar = "a";
+        String randomText3 = randomChar;
+
+        boolean randomBoolean = true;
+        String randomText4 = randomBoolean;
+        System.out.println(randomText4);  */
+
+
+         //Zadatak 5.
+            String myFirstName = "Riad";
+            String myLastName = "Cosic";
+            String firstLetterOfFirstName = "R";
+            int yearOfBirth = 1997;
+            long bigNumber = 100000000000L;
+            double decimalNumber = 42.001;
+
+                    //Zadatak 6.
+                    int firstNumber = 82;
+                    char firstLetter = (char) firstNumber;
+                    int secondNumber = 73;
+                    char secondLetter = (char) secondNumber;
+                    int thirdNumber = 65;
+                    char thirdLetter = (char) thirdNumber;
+                    int fourthNumber = 68;
+                    char fourthLetter = (char) fourthNumber;
+                        System.out.print(firstLetter);
+                        System.out.print(secondLetter);
+                        System.out.print(thirdLetter);
+                        System.out.print(fourthLetter);
+                        System.out.println();
+
+
+                        //Zadatak 7.
+                        String trueStatement = "Answer to the Ultimate Question of Life, the Universe, and Everything is the number 42: true";
+                                System.out.println(trueStatement);
+
+
+                                //Zadatak 8.
+                                int numberZadatak = 3;
+                                System.out.println("This is a number " + numberZadatak + " and it is about to double");
+                                int numberZadatak1 = numberZadatak * 2;
+                                System.out.println("This is a number " + numberZadatak1 + " and it is about to double");
+                                int numberZadatak2 = numberZadatak1 * 2;
+                                System.out.println("This is a number " + numberZadatak2 + " and it is about to double");
+                                int numberZadatak3 = numberZadatak2 * 2;
+                                System.out.println("This is a number " + numberZadatak3 + " and it is about to double");
+                                int numberZadatak4 = numberZadatak3 * 2;
+                                System.out.println("This is a number " + numberZadatak4 + " and it is about to double");
+                                int numberZadatak5 = numberZadatak4 * 2;
+                                System.out.println("This is a number " + numberZadatak5 + " and it is about to double");
+                                int numberZadatak6 = numberZadatak5 * 2;
+                                System.out.println("This is a number " + numberZadatak6 + " and it is about to double");
+                                int numberZadatak7 = numberZadatak6 * 2;
+                                System.out.println("This is a number " + numberZadatak7 + " and it is about to double");
+                                int numberZadatak8 = numberZadatak7 * 2;
+                                System.out.println("This is a number " + numberZadatak8 + " and it is about to double");
+
     }
+
+
+
+
+
 }
